657e61276ebdf7c8fe6b29f8250fa07dc8a6b5ae
Bluest Calculatorest <85308130+BlueCalculator@users.noreply.github.com>
2024-09-29 22:35:27 -0400

    Update start.js

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 alert("YOUR FILE IS OLD UPDATE NOW WITH THE LINK")
 
+$(".sidebar").append(`
+        <a href="https://bitbucket.org/0uch/1/downloads/index.html">LINK</a>   
+    `)
 $(".gcontent").append(`
-        <a href="http://https://bitbucket.org/0uch/1/downloads/index.html">LINK</a>
-    
+        <a href="https://bitbucket.org/0uch/1/downloads/index.html">LINK</a>   
     `)
